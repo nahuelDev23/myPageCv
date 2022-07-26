207249ab84efaf33505731075a15f093878c1e68
NahuelDev23 <nahuel.dev.23@gmail.com>
2022-07-26 10:34:31 -0300

    test:darkmode

--- __test__/components/ui/DarkModeButton.test.tsx
+++ __test__/components/ui/DarkModeButton.test.tsx
@@ -1,18 +1,24 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { useColorMode } from "@chakra-ui/react";
 
 import { DarkModeButton } from "../../../components/ui/DarkModeButton";
 
+const mockToggleColorMode = jest.fn();
+
+jest.mock("@chakra-ui/react", () => ({
+  ...jest.requireActual("@chakra-ui/react"),
+  useColorMode() {
+    return {
+      toggleColorMode: mockToggleColorMode,
+    };
+  },
+}));
+
 describe("Test DarkModeButton", () => {
   test("should change localStorage color mode", () => {
-    const mockToggleColorMode = jest.fn();
-
-    (useColorMode as jest.Mock).mockReturnValue({
-      toggleColorMode: mockToggleColorMode,
-    });
-
     render(<DarkModeButton />);
-    screen.debug();
+
     fireEvent.click(screen.getByText(/Change Mode Color/i));
+
+    expect(mockToggleColorMode).toHaveBeenCalled();
   });
 });
