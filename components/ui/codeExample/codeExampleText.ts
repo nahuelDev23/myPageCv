25914e6addabd00f94ff3673da377a5fa1f88862
NahuelDev23 <nahuel.dev.23@gmail.com>
2022-07-28 15:12:18 -0300

    fix:ident

--- components/ui/codeExample/codeExampleText.ts
+++ components/ui/codeExample/codeExampleText.ts
@@ -1,16 +1,16 @@
 const badCode = `// bad 
 export class Pokemon {
 
-    constructor(
-        public readonly id: number, 
-        public name: string,
-    ) {}
+ constructor(
+  public readonly id: number, 
+  public name: string,
+  ) {}
 
-    async getMoves(): Promise<Move[]> {
-        const url = \`https://pokeapi.co/api/v2/pokemon/\${id}\`
-        const { data } = await axios.get<PokeapiResponse>(url);
-        return data.moves;
-    }
+ async getMoves(): Promise<Move[]> {
+  const url = \`https://pokeapi.co/api/v2/pokemon/\${id}\`
+  const { data } = await axios.get<PokeapiResponse>(url);
+  return data.moves;
+  }
 
 }
 
@@ -23,23 +23,26 @@ charmander.getMoves();
 const goodCode = `// better
 export class Pokemon {
 
-    constructor(
-      public readonly id: number,
-      public name: string,
-      private readonly http: HttpAdapter,
-    ) { }
+ constructor(
+  public readonly id: number,
+  public name: string,
+  private readonly http: HttpAdapter,
+) { }
 
-    async getMoves(): Promise < Move[] > {
-        const url = \`https://pokeapi.co/api/v2/pokemon/\${id}\`
-        const data = await this.http.get<PokeapiReponse>(url);
-        return data.moves;
-    }
+async getMoves(): Promise < Move[] > {
+  const url = \`https://pokeapi.co/api/v2/pokemon/\${id}\`
+  const data = await this.http.get<PokeapiReponse>(url);
+  return data.moves;
+ }
 }
 
 const pokeApiAxios = new PokeApiAdapter();
 const pokeApiFetch = new PokeApiFetchAdapter();
 
-export const charmander = new Pokemon(4, 'Charmander', pokeApiAxios);
+export const charmander = new Pokemon(
+    4, 
+    'Charmander',
+    pokeApiAxios);
 
 charmander.getMoves();
 
